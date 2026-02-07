b47aeb575957704db5cdc84167c52c19c6061b63
thierryallen <allt5140@usherbrooke.ca>
2026-02-07 10:07:31 -0500

    fixed animation for link to cinematic

--- Trajectoire/plannif_trajectoire/config_traj.py
+++ Trajectoire/plannif_trajectoire/config_traj.py
@@ -12,7 +12,7 @@ speed_approach_move_global = 15.0  # cm/s
 # Zone valide Z approx: [-20, -45] selon l'éloignement du centre.
 
 z_table = -30.0       # Hauteur de la table adaptée pour Lc=15/Lb=32
-z_home = -25.0        # Hauteur de sécurité (Home)
+z_home = -15.0        # Hauteur de sécurité (Home)
 
 home_position = (0.0, 0.0, z_home)
 
@@ -28,13 +28,13 @@ yellow_output_position = (0.0, -offset_output, z_table)
 
 # Fixed Z values within [z_table, z_home] (no randomness)
 blocs = np.array([
-    ("red",    "2x4",   0.0,   0.0, -30.0),
+    ("red",    "2x4",   -10.0,   2.0, -30.0),
     ("blue",   "2x2",  8.0,   0.0, -30.0),
-    ("green",  "2x4",   0.0,  8.0, -30.0),
+    ("green",  "2x4",   6.0,  8.0, -30.0),
     ("yellow", "2x2", -9.0, -7.0, -30.0),
     ("red",    "2x4",  4.0,  -5.0, -30.0),
     ("yellow", "2x8", -9.0,  8.0, -30.0),
-    ("green",  "1x4",   5.0,  6.0, -30.0),
+    ("green",  "1x4",   -5.0,  6.0, -30.0),
     ("blue",   "2x2",  5.0,  6.0, -30.0),
     ("red",    "2x2",  -5.0,   0.0, -30.0)
 ], dtype=object)
