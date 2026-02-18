1d9d99a0871ff4ea049cb356b7a12fad93408dbb
thierryallen <allt5140@usherbrooke.ca>
2026-02-18 11:43:29 -0500

    better rgb detection

--- Trajectoire/plannif_trajectoire/config_traj.py
+++ Trajectoire/plannif_trajectoire/config_traj.py
@@ -28,9 +28,9 @@ yellow_output_position = (0.0, -offset_output, z_table)
 
 # Fixed Z values within [z_table, z_home] (no randomness)
 blocs = np.array([
-    ("red",    "2x4",  4.0,  -5.0, -30.0),
-    ("yellow", "2x8", -9.0,  8.0, -30.0),
-    ("green",  "1x4",   -5.0,  6.0, -30.0),
-    ("blue",   "2x2",  5.0,  6.0, -30.0),
-    ("red",    "2x2",  -5.0,   0.0, -30.0)
+    ("red","2x4",  4.0,  -5.0, -30.0),
+    ("yellow","2x8", -9.0,  8.0, -30.0),
+    ("green","1x4",   -5.0,  6.0, -30.0),
+    ("blue","2x2",  5.0,  6.0, -30.0),
+    ("red","2x2",  -5.0,   0.0, -30.0)
 ], dtype=object)
