788bb75d2efef357a4a2d00a15b08d8afb3da737
thierryallen <allt5140@usherbrooke.ca>
2026-02-24 21:23:36 -0500

    Fixed graph and animation for trajectoire

--- Trajectoire/plannif_trajectoire/config_traj.py
+++ Trajectoire/plannif_trajectoire/config_traj.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # --- Paramètres globaux ---
 MAX_BLOCS_OPTIMAL = 7
-speed_joint_move_global = 70.0  # cm/s
+speed_joint_move_global = 40.0  # cm/s
 speed_approach_move_global = 15.0  # cm/s
 
 # --- Coordonnées du Robot (Centimètres) ---
@@ -19,18 +19,21 @@ home_position = (0.0, 0.0, z_home)
 # Positions de sortie (Output)
 # On réduit l'écartement car le robot est plus petit
 offset_output = 8.0
-red_output_position = (offset_output, 0.0, z_table)
-blue_output_position = (-offset_output, 0.0, z_table)
-green_output_position = (0.0, offset_output, z_table)
-yellow_output_position = (0.0, -offset_output, z_table)
+red_output_position = (8.5, 5.0, z_table)
+blue_output_position = (-9.0, 0.0, z_table)
+green_output_position = (0.0, 10.0, z_table)
+yellow_output_position = (0.0, -10.0, z_table)
 
 # --- Blocs ---
 
 # Fixed Z values within [z_table, z_home] (no randomness)
 blocs = np.array([
-    ("red","2x4",  4.0,  -5.0, -30.0),
-    ("yellow","2x8", -9.0,  8.0, -30.0),
-    ("green","1x4",   -5.0,  6.0, -30.0),
-    ("blue","2x2",  5.0,  6.0, -30.0),
-    ("red","2x2",  -5.0,   0.0, -30.0)
+    ("red","2x4",  4.0,  -5.0, -40.0),
+    ("yellow","2x8", -7.0,  8.0, -40.0),
+    ("green","1x4",   -5.0,  6.0, -40.0),
+    ("blue","2x2",  5.0,  5.0, -40.0),
+    ("red","2x2",  -5.0,   0.0, -40.0),
+    ("yellow","2x8", 10.0,  0.5, -40.0),
+    ("green","1x4",   2.0,  2.0, -40.0),
+    ("blue","2x2",  -5.0,  10.0, -40.0)
 ], dtype=object)
